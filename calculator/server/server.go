6aa2e9f4db5ed1c4a64a88f94b5a9b1445aa1f2f
hlvudat1206 <hlvudat1206@gmail.com>
2024-11-04 19:51:41 +0700

    update

---
+++ calculator/server/server.go
@@ -0,0 +1,24 @@
+package main
+import (
+	"log"
+	"net"
+
+)
+
+type server struct {}
+
+func main() {
+	lis, err:= net.Listen("tcp", "0.0.0.0:50069")
+	if err != nil {
+		log.Fatalf("err while create listen %v",err)
+	}
+
+	s: grpc.NewServer()
+	calculatorpb.RegisterCalculatorServiceServer(s, &server{})
+
+	err: = s.Serve(lis)
+	if err != nil {
+		log.Fatalf("err while serve %v",err)
+	}
+
+}
